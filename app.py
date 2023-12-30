80ea5bd3c9334ccbb300715706cb0edf5156293f
moududur <sfgrahman35@gmail.com>
2023-12-30 10:23:25 +0600

    update

--- app.py
+++ app.py
@@ -14,3 +14,9 @@ if question:
 
     st.header("Answer")
     st.write(response["result"])
+st.markdown("""Few example questions:
+            1. Who does mental illness affect?
+            2. What treatment options are available?
+            3. What is the difference between mental health professionals?
+            
+            """)
